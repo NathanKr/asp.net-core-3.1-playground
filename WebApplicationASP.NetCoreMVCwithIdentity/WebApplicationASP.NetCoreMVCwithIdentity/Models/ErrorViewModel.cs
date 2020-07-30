bae352d5e23b79884f5f8b1148c59478c44c8654
Nathan Krasney <natankrasney@gmail.com>
2020-07-30 12:24:39 +0300

    some initial version

---
+++ WebApplicationASP.NetCoreMVCwithIdentity/WebApplicationASP.NetCoreMVCwithIdentity/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebApplicationASP.NetCoreMVCwithIdentity.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
