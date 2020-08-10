4fda895e3aa36a57935f61738dfb3c6fbb9f8d87
Nathan Krasney <natankrasney@gmail.com>
2020-08-10 14:34:11 +0300

    DI and DbContext

---
+++ WebApplicationRazorLibrary/WebApplicationRazorLibrary/Logic/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplicationRazorLibrary.Models;
+
+namespace WebApplicationRazorLibrary.Logic
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        // --- represent books table
+        public DbSet<Book> Books{ get; set; }
+    }
+}
